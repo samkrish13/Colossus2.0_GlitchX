eeb3330ce6f80fcab154e1d5957c97f48ff4c4e4
samkrish13 <135528839+samkrish13@users.noreply.github.com>
2025-04-12 04:53:53 +0530

    Update emotion_model.py

--- ai/emotion_model.py
+++ ai/emotion_model.py
@@ -1,10 +1,17 @@
 from fer import FER
 import cv2
 
-# Load the emotion detector model once
 detector = FER(mtcnn=True)
 
 def detect_emotion(frame):
- 
-    emotion, score = detector.top_emotion(frame)
-    return emotion, score
+    
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Detect emotions
+    results = detector.detect_emotions(rgb_frame)
+    
+    if results:
+        top_emotion = detector.top_emotion(rgb_frame)
+        return top_emotion  # Returns tuple (emotion, confidence)
+    else:
+        return None, 0
