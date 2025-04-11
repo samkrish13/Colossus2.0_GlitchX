48998f32a061c39464fce6871f34ea1efd395fb3
samkrish13 <135528839+samkrish13@users.noreply.github.com>
2025-04-11 21:39:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
+from config import Config
+from db import db, init_db
 from routes.emotion_routes import emotion_routes
 from routes.learning_routes import learning_routes
 
 app = Flask(__name__)
+app.config.from_object(Config)
+CORS(app)
 
+# Initialize DB
+init_db(app)
+
+# Register Blueprints
 app.register_blueprint(emotion_routes, url_prefix="/api/emotion")
 app.register_blueprint(learning_routes, url_prefix="/api/learn")
 
