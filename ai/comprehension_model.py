62b01c2d3ed19a95c76e504cbbba983ba0d848a4
samkrish13 <135528839+samkrish13@users.noreply.github.com>
2025-04-11 21:53:35 +0530

    Update comprehension_model.py

--- ai/comprehension_model.py
+++ ai/comprehension_model.py
@@ -1,4 +1,3 @@
-
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
@@ -7,6 +6,7 @@ def estimate_comprehension(text_response):
     result = classifier(text_response)[0]
     label = result["label"]
     score = result["score"]
+
     if label == "POSITIVE":
         return min(1.0, score + 0.1)
     return max(0.0, 1 - score)
