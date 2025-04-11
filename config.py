13f2b7cae91bb1809d8497f22441cce897e771db
samkrish13 <135528839+samkrish13@users.noreply.github.com>
2025-04-11 21:39:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,15 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_key"
+    
     MYSQL_USER = os.environ.get("DB_USER") or "root"
     MYSQL_PASSWORD = os.environ.get("DB_PASSWORD") or "password"
     MYSQL_DB = os.environ.get("DB_NAME") or "reverse_classroom"
     MYSQL_HOST = os.environ.get("DB_HOST") or "localhost"
     MYSQL_PORT = int(os.environ.get("DB_PORT") or 3306)
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
